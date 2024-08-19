38777db47e0f31a4772aa2ec85fe52b08b5b4b36
ForgetMeNot152 <nikitamks@yandex.ru>
2024-08-19 22:12:44 +0300

    feat:add this tasc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
-}
+        const units = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine'];
+        const tens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+      
+        let result = '';
+
+        if (number === 0) {
+            return 'zero';
+          }
+      
+        if (number >= 100) {
+          result += units[Math.floor(number / 100)] + ' hundred';
+          number %= 100;
+        }
+      
+        if (number >= 20) {
+          result += tens[Math.floor(number / 10)];
+          number %= 10;
+        }
+      
+        if (number >= 10) {
+          result += [' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'][number - 10];
+
+
+        } else {
+          result += units[number];
+        }
+      
+        return result.trim();
+      }
+      
+;
+      
+
+console.log(module.exports(0))
